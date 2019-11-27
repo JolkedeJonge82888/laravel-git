493a90c97f6dadb10db85d2297126bea0283546b
Jolke <82888@glr.nl>
2019-11-27 14:02:58 +0100

    env file

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -29,30 +29,33 @@ class HomeController extends Controller
 
     public function index()
     {
-        DB::enableQueryLog();
-
 
 
         DB::enableQueryLog(); // Enable query log
         $team = Users::find(Auth::user()->id)->Team->pluck('id');
+
         if($team == null) {
 
             return view('home');
 
         } else {
-            $user = Team::find($team)->Users->pluck('name');
+            //dd($team);
+//            $user = Team::find($team->id)->Users->pluck('name');
+
+            $users = Team::where('id', '=', $team)->Users->get();
             dd(DB::getQueryLog());
+            //dd($users);
 //            $teamMembers = Teams::join('Team_student', 'Team.ID', '=', 'Team_student.TeamID')
 //                ->join('users', 'Team_student.userID', '=', 'users.id')
 //                ->select('users.name', 'users.username')
 //                ->where('Team.ID', '=', $team)
 //                ->get();
 
-            return view('home', ['team' => $teamMembers]);
+            return view('home', ['users' => $user]);
         }
 
         //dd($team);
-        //dd(DB::getQueryLog());
+        //
 
     }
 
