28d6bdd954d6e3884d57ee6a680a164e8f7d25c4
rick <82729@glr.nl>
2019-12-04 13:40:14 +0100

    javascript statistics

--- resources/views/opdracht.blade.php
+++ resources/views/opdracht.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-    @foreach ($opdrachten as $opdracht)
+{{--    @foreach ($opdrachten as $opdracht)--}}
         <div class="col-xs-12 col-sm-6 col-md-4">
             <div class="image-flip">
                 <div class="mainflip">
@@ -9,8 +9,8 @@
                         <div class="card">
                             <div class="card-body text-center">
                                 <p><img class=" img-fluid" src="" alt="card image"></p>
-                                <h4 class="card-title">{{ $opdracht->title }}</h4>
-                                <p class="card-text">{{ $opdracht->describtion }}</p>
+                                <h4 class="card-title">Test{{-- $opdracht->title --}}</h4>
+                                <p class="card-text">Meer Test{{-- $opdracht->describtion --}}</p>
                                 <a href="#" class="btn btn-primary btn-sm"><i class="fa fa-plus"></i></a>
                             </div>
                         </div>
@@ -18,5 +18,5 @@
                 </div>
             </div>
         </div>
-    @endforeach
+{{--    @endforeach--}}
 @endsection
