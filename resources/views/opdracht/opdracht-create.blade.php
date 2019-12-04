343e7e912ffa7be4de49229cb2de8dc8d921adb8
Jolke <82888@glr.nl>
2019-12-04 13:36:54 +0100

    Adding create opdracht function

---
+++ resources/views/opdracht/opdracht-create.blade.php
@@ -0,0 +1,45 @@
+@extends('layouts.app')
+
+@section('content')
+    <style>
+        .uper {
+            margin-top: 40px;
+        }
+    </style>
+    <div class="card uper">
+        <div class="card-header">
+            Add Opdracht
+        </div>
+        <div class="card-body">
+            @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div><br />
+            @endif
+            <form method="post">
+                <div class="form-group">
+                    @csrf
+                    <label for="name">Opdracht Title:</label>
+                    <input type="text" class="form-control" name="opdracht_title"/>
+                </div>
+                <div class="form-group">
+                    <label for="price">Opdracht Description:</label>
+                    <textarea class="form-control" rows="5" name="opdracht_title"></textarea>
+                </div>
+                <div class="form-group">
+                    <label for="quantity">Opdracht Start Date:</label>
+                    <input type="date" class="form-control" placeholder="mm-dd-yy" name="opdracht_startdate"/>
+                </div>
+                <div class="form-group">
+                    <label for="quantity">Opdracht End Date:</label>
+                    <input type="date" class="form-control" placeholder="mm-dd-yy" name="opdracht_enddate"/>
+                </div>
+                <button type="submit" class="btn btn-primary">Add Opdracht</button>
+            </form>
+        </div>
+    </div>
+@endsection
