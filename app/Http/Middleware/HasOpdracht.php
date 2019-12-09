4fa803ebe6aa8e2b90cbc92b2644c99c491a9bb4
Jolke <82888@glr.nl>
2019-12-09 13:17:46 +0100

    Adding show and adding HasOpdracht blade function!

--- app/Http/Middleware/HasOpdracht.php
+++ app/Http/Middleware/HasOpdracht.php
@@ -4,7 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 
-class IsAdmin
+class HasOpdracht
 {
     /**
      * Handle an incoming request.
@@ -15,7 +15,7 @@ class IsAdmin
      */
     public function handle($request, Closure $next)
     {
-        if(auth()->user()->isAdmin()) {
+        if(auth()->user()->hasOpdracht()) {
             return $next($request);
         }
         return redirect('home');
