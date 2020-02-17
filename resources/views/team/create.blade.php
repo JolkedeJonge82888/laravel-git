1f25b8a33a08688793335a0bc1561f9b3318bcfa
rick <82729@glr.nl>
2020-02-17 13:57:54 +0100

    add team

--- resources/views/team/create.blade.php
+++ resources/views/team/create.blade.php
@@ -1,1 +1,21 @@
-<?php
+@extends('layouts.app')
+
+@section('content')
+    <div class="card uper">
+        <div class="card-header">
+            Add team
+        </div>
+        <div class="card-body">
+
+            <form method="POST" action="{{ route('team.store') }}">
+                <div class="form-group">
+                    @csrf
+                    <label class="" for="name">Team Name:</label>
+                    <input type="text" class="form-control teamName" name="team_title" value="Team Name"/>
+                    <br>
+                </div>
+                <button type="submit" class="btn btn-primary">Add Team</button>
+            </form>
+        </div>
+    </div>
+@endsection
